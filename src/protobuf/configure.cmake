63b9cc81df95a16070c2a475296774a294019170
Alexey Edelev <alexey.edelev@qt.io>
2023-01-30 19:15:14 +0100

    Rearrange qtprotobufgen and qtgrpcgen features
    These features should only be enabled when the corresponding tools
    should be built. Currently features are enabled even if tools won't
    be built. This leads to the following issues when crosscompiling and
    the host Qt lacks the qtprotobufgen/qtgrpcgen targets:
      Failed to find the host tool "Qt6::qtprotobufgen".  It is part of the
      Qt6ProtobufTools package, but the package could not be found.
    
    Instead of enabling features when crosscompiling by default, search for
    the module host tool targets first in top-level CMakeLists.txt and
    next check if respective tools can be found. Make the
    qt_internal_add_tool calls dependent on either the
    qtprotobufgen/qtgrpcgen features or the host tools lookup results.
    
    This also adds the qtgrpcgen feature since it makes sense to skip
    building the gRPC generator using separate feature.
    
    Add the missing dependencies on qtgrpc to the qtgrpcgen and native_grpc
    features. Both features now are disabled if the grpc feature is OFF.
    
    Remove redundant feature checks and replace them with tool target check
    where is necessary.
    
    TODO: QT_FORCE_BUILD_TOOLS doesn't lead to the error if the
    dependencies of the repo tools are not satisfied. See QTBUG-110849
    for details.
    
    Amends 326ebfaadbb571c30068456f13af2d3acb0d5934
    
    Fixes: QTBUG-110704
    Pick-to: 6.5
    Change-Id: Ib29c4b24bc3e8c5147bf40332909bd8d44664699
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/protobuf/configure.cmake
+++ src/protobuf/configure.cmake
@@ -7,14 +7,20 @@ qt_find_package(WrapProtobuf
         WrapProtobuf::WrapLibProtobuf
     MODULE_NAME global
 )
+
+# WrapProtoc::WrapProtoc could come from top-level CMakeLists.txt so avoid promoting it to GLOBAL
+# here in this case.
+if(TARGET WrapProtoc::WrapProtoc)
+    qt_internal_disable_find_package_global_promotion(WrapProtoc::WrapProtoc)
+endif()
 qt_find_package(WrapProtoc PROVIDED_TARGETS WrapProtoc::WrapProtoc MODULE_NAME global)
 
 qt_feature("qtprotobufgen" PRIVATE
     SECTION "Utilities"
     LABEL "Qt Protobuf generator"
     PURPOSE "Provides support for generating Qt-based classes for use with Protocol Buffers."
-    CONDITION (TARGET WrapProtobuf::WrapLibProtoc AND TARGET WrapProtobuf::WrapLibProtobuf AND
-        TARGET WrapProtoc::WrapProtoc) OR (CMAKE_CROSSCOMPILING AND NOT QT_FORCE_BUILD_TOOLS)
+    CONDITION TARGET WrapProtobuf::WrapLibProtoc AND TARGET WrapProtobuf::WrapLibProtobuf AND
+        TARGET WrapProtoc::WrapProtoc
 )
 
 qt_configure_add_summary_section(NAME "Qt Protobuf tools")
