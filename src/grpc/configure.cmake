39f2a1e64a52720bf758b5a7e5444a82656d6a18
Alexey Edelev <alexey.edelev@qt.io>
2023-01-30 19:15:14 +0100

    Rearrange qtprotobufgen and qtgrpcgen features
    These features should only be enabled when the corresponding tools
    should be built. Currently features are enabled even if tools won't
    be built. This leads to the following issues when crosscompiling and
    the host Qt lacks the qtprotobufgen/qtgrpcgen targets:
      Failed to find the host tool "Qt6::qtprotobufgen".  It is part of the
      Qt6ProtobufTools package, but the package could not be found.
    
    Instead of enabling features when crosscompiling by default, search for
    the module host tool targets first in top-level CMakeLists.txt and
    next check if respective tools can be found. Make the
    qt_internal_add_tool calls dependent on either the
    qtprotobufgen/qtgrpcgen features or the host tools lookup results.
    
    This also adds the qtgrpcgen feature since it makes sense to skip
    building the gRPC generator using separate feature.
    
    Add the missing dependencies on qtgrpc to the qtgrpcgen and native_grpc
    features. Both features now are disabled if the grpc feature is OFF.
    
    Remove redundant feature checks and replace them with tool target check
    where is necessary.
    
    TODO: QT_FORCE_BUILD_TOOLS doesn't lead to the error if the
    dependencies of the repo tools are not satisfied. See QTBUG-110849
    for details.
    
    Amends 326ebfaadbb571c30068456f13af2d3acb0d5934
    
    Fixes: QTBUG-110704
    Change-Id: Ib29c4b24bc3e8c5147bf40332909bd8d44664699
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 63b9cc81df95a16070c2a475296774a294019170)

--- src/grpc/configure.cmake
+++ src/grpc/configure.cmake
@@ -5,14 +5,25 @@ qt_find_package(WrapgRPC PROVIDED_TARGETS WrapgRPC::WrapLibgRPC MODULE_NAME
                 global)
 
 qt_feature("grpc" PUBLIC
-  SECTION "Utilities"
-  LABEL "gRPC support"
-  PURPOSE "Provides support for serializing and deserializing gRPC services using Qt."
-  CONDITION TARGET Qt6::Network)
+    SECTION "Utilities"
+    LABEL "gRPC support"
+    PURPOSE "Provides support for serializing and deserializing gRPC services using Qt."
+    CONDITION TARGET Qt6::Network
+)
+
+qt_feature("qtgrpcgen" PRIVATE
+    SECTION "Utilities"
+    LABEL "Qt GRPC generator"
+    PURPOSE "Provides support for generating Qt-based gRPC services."
+    DISABLE NOT QT_FEATURE_grpc
+    CONDITION TARGET WrapProtobuf::WrapLibProtoc AND TARGET WrapProtobuf::WrapLibProtobuf AND
+        TARGET WrapProtoc::WrapProtoc
+)
 
 qt_feature("native_grpc" PUBLIC
     SECTION "Utilities"
     LABEL "Native gRPC support"
+    DISABLE NOT QT_FEATURE_grpc
     PURPOSE "Provides native implementation for gRPC channels and client and service code generation."
     CONDITION TARGET WrapgRPC::WrapLibgRPC
 )
@@ -20,4 +31,7 @@ qt_feature("native_grpc" PUBLIC
 qt_configure_add_summary_section(NAME "Qt GRPC")
 qt_configure_add_summary_entry(ARGS "grpc")
 qt_configure_add_summary_entry(ARGS "native_grpc")
+qt_configure_end_summary_section()
+qt_configure_add_summary_section(NAME "Qt GRPC tools")
+qt_configure_add_summary_entry(ARGS "qtgrpcgen")
 qt_configure_end_summary_section()
