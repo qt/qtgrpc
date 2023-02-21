20c7e86ed0917d4b92517650ac0f724298246639
Konrad Kujawa <konrad.kujawa@qt.io>
2023-02-21 15:32:56 +0100

    Add missing documentation of QAbstractGrpcChannel
    Task-number: QTBUG-103235
    Change-Id: I4b8cb9c4a903183233b41841984a4a8d45468d87
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit baae5ec639ca60ed69834dac281fdf7f0480eb31)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/grpc/qabstractgrpcchannel.cpp
+++ src/grpc/qabstractgrpcchannel.cpp
@@ -14,6 +14,58 @@ QT_BEGIN_NAMESPACE
     gRPC channel functionality.
 
     You may implement this interface to create own channels for gRPC transport.
+    QGrpcChannel or QGrpcHttp2Channel, which are full implementations of
+    QAbstractGrpcChannel are recommended to use.
+*/
+
+/*!
+    \fn virtual QGrpcStatus QAbstractGrpcChannel::call(QLatin1StringView method, QLatin1StringView service,
+                                                        QByteArrayView args, QByteArray &ret) = 0
+
+    This pure virtual function synchronously calls the RPC method concatenated
+    from the \a method and \a service parameters with the given \a args
+    and writes the result to the output parameter \a ret.
+
+    You may reimplement this function in a subclass to define your own call
+    mechanism behavior. QGrpcChannel or QGrpcHttp2Channel, which are full
+    implementations of QAbstractGrpcChannel are recommended to use.
+*/
+
+/*!
+    \fn virtual std::shared_ptr<QGrpcCallReply> QAbstractGrpcChannel::call(QAbstractGrpcClient *client,
+                                                                        QLatin1StringView method,
+                                                                        QLatin1StringView service,
+                                                                        QByteArrayView args) = 0
+
+    This pure virtual function asynchronously calls the RPC method combined
+    with the \a method and \a service parameters with the given \a args
+    and returns an asynchronous response in the form of QGrpcCallReply,
+    which is owned by the \a client.
+
+    You may reimplement this function in a subclass to define your own call
+    mechanism behavior. QGrpcChannel or QGrpcHttp2Channel, which are full
+    implementations of QAbstractGrpcChannel are recommended to use.
+*/
+
+/*!
+    \fn virtual void QAbstractGrpcChannel::startStream(QGrpcStream *stream, QLatin1StringView service) = 0
+
+    This pure virtual function starts a stream on a \a stream using
+    QGrpcStream::method() and the \a service to get the name of the RPC method.
+
+    You may reimplement this function in a subclass to define your own stream
+    mechanism behavior. QGrpcChannel or QGrpcHttp2Channel, which are full
+    implementations of QAbstractGrpcChannel are recommended to use.
+*/
+
+/*!
+    \fn virtual std::shared_ptr<QAbstractProtobufSerializer> QAbstractGrpcChannel::serializer() const = 0
+
+    This pure virtual function shall return a shared pointer
+    to QAbstractProtobufSerializer.
+
+    This function is called to obtain the QAbstractProtobufSerializer used by
+    QAbstractGrpcClient to perform serialization and deserialization of the message.
 */
 
 QAbstractGrpcChannel::QAbstractGrpcChannel() : dPtr(std::make_unique<QAbstractGrpcChannelPrivate>())
