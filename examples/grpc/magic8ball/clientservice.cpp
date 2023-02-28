407a2760c8e0d92a8393ff7f14a85186ed43c95b
Tatiana Borisova <tatiana.borisova@qt.io>
2023-02-28 19:22:39 +0100

    Window Material theme should be always light for magic8ball app
    Plus, make clientserver code more readable
    
    Task-number: QTBUG-109598
    Change-Id: If8f5c6779a8114308fcc02cf517ce602050a4f68
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit fb47ac8040e97557599173ec474c9cbae2a3a996)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/grpc/magic8ball/clientservice.cpp
+++ examples/grpc/magic8ball/clientservice.cpp
@@ -13,16 +13,13 @@ ClientService::ClientService(QObject *parent) :
     m_client(new ExampleService::Client),
     m_response(new AnswerResponse)
 {
-    connect(m_client.get(), &qtgrpc::examples::ExampleService::Client::errorOccurred,
+    connect(m_client.get(), &ExampleService::Client::errorOccurred,
             this, &ClientService::errorOccurred);
 
-    auto channel = std::shared_ptr<QAbstractGrpcChannel>(new QGrpcHttp2Channel(
-                                                             QUrl("http://localhost:50051",
-                                                                  QUrl::StrictMode),
-                                                             QGrpcInsecureChannelCredentials()
-                                                             | QGrpcInsecureCallCredentials()));
-
-    m_client->attachChannel(channel);
+    QUrl url("http://localhost:50051", QUrl::StrictMode);
+    m_client->attachChannel(std::make_shared<QGrpcHttp2Channel>(url,
+                                                                QGrpcInsecureChannelCredentials()
+                                                                | QGrpcInsecureCallCredentials()));
 }
 
 void ClientService::errorOccurred()
@@ -38,7 +35,7 @@ void ClientService::sendRequest()
     // clean error on UI before new request
     emit errorRecieved("");
 
-    qtgrpc::examples::AnswerRequest request;
+    AnswerRequest request;
     request.setMessage("sleep");
 
     m_client->answerMethod(request, m_response.get());
