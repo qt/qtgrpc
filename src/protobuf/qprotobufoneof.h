cfd0591d4eadf388a17a8e709fc0a8e99e2735a3
Alexey Edelev <alexey.edelev@qt.io>
2023-03-03 12:35:59 +0100

    Fix the uint64 support by oneof fields
    QtProtobuf::uint64 support was missing because of typo in the
    IsNonMessageProtobufType type trait.
    
    Amends 2e3fdd5c05b83b0f9a421f1a71061bcec419596e
    
    Change-Id: I5647a15acbbd342dea2cc08572a46777953f2e8f
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- src/protobuf/qprotobufoneof.h
+++ src/protobuf/qprotobufoneof.h
@@ -37,7 +37,7 @@ class Q_PROTOBUF_EXPORT QProtobufOneof final
             std::disjunction_v<
                     std::is_same<T, QtProtobuf::int32>, std::is_same<T, QtProtobuf::int64>,
                     std::is_same<T, QtProtobuf::sint32>, std::is_same<T, QtProtobuf::sint64>,
-                    std::is_same<T, QtProtobuf::uint32>, std::is_same<T, QtProtobuf::sint32>,
+                    std::is_same<T, QtProtobuf::uint32>, std::is_same<T, QtProtobuf::uint64>,
                     std::is_same<T, QtProtobuf::boolean>, std::is_same<T, QtProtobuf::fixed32>,
                     std::is_same<T, QtProtobuf::fixed64>, std::is_same<T, QtProtobuf::sfixed32>,
                     std::is_same<T, QtProtobuf::sfixed64>, std::is_same<T, float>,
