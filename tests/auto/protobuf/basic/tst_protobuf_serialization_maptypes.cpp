c3b04ea59ce2761fc5e8af23093066dc28a073ab
Konrad Kujawa <konrad.kujawa@qt.io>
2023-03-08 13:20:59 +0100

    Split tst_protobuf_serialization_maptypes tests
    Split tst_protobuf_serialization_maptypes into
    tst_protobuf_serialization_maptypes and
    tst_protobuf_serialization_complex_maptypes.
    Fix formatting in those tests.
    
    Task-number: QTBUG-111070
    Change-Id: I7add0a9eab4503fe87d746ceaddec062d8993603
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 4560309429ce126bfecc7f64e163320a64005d1f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/protobuf/basic/tst_protobuf_serialization_maptypes.cpp
+++ tests/auto/protobuf/basic/tst_protobuf_serialization_maptypes.cpp
@@ -2,25 +2,44 @@
 // Copyright (C) 2022 Alexey Edelev <semlanik@gmail.com>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "tst_protobuf_serialization_maptypes.h"
-
-#include "basicmessages.qpb.h"
 #include "mapmessages.qpb.h"
 
 #include <qtprotobuftestscommon.h>
 
+#include <QObject>
+#include <QProtobufSerializer>
 #include <QTest>
 
 using namespace qtprotobufnamespace::tests;
 
+class QtProtobufMapTypesSerializationTest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init() { m_serializer.reset(new QProtobufSerializer); }
+    void SimpleFixed32StringMapSerializeTest();
+    void SimpleSFixed32StringMapSerializeTest();
+    void SimpleInt32StringMapSerializeTest();
+    void SimpleSInt32StringMapSerializeTest();
+    void SimpleUInt32StringMapSerializeTest();
+    void SimpleFixed64StringMapSerializeTest();
+    void SimpleSFixed64StringMapSerializeTest();
+    void SimpleInt64StringMapSerializeTest();
+    void SimpleSInt64StringMapSerializeTest();
+    void SimpleUInt64StringMapSerializeTest();
+    void SimpleStringStringMapSerializeTest();
+
+private:
+    std::unique_ptr<QProtobufSerializer> m_serializer;
+};
+
 void QtProtobufMapTypesSerializationTest::SimpleFixed32StringMapSerializeTest()
 {
     SimpleFixed32StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {42, {"fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { 42, { "fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "3a0a0d0a000000120374656e",
+    QVERIFY2(compareSerializedChunks(result.toHex(), "3a0a0d0a000000120374656e",
                                      "3a0e0d0f00000012076669667465656e",
                                      "3a110d2a000000120a666f757274792074776f"),
              result.toHex());
@@ -29,11 +48,11 @@ void QtProtobufMapTypesSerializationTest::SimpleFixed32StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleSFixed32StringMapSerializeTest()
 {
     SimpleSFixed32StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {-42, {"minus fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { -42, { "minus fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
     QVERIFY2(compareSerializedChunks(result.toHex(),
-                                    "4a170dd6ffffff12106d696e757320666f757274792074776f",
+                                     "4a170dd6ffffff12106d696e757320666f757274792074776f",
                                      "4a0a0d0a000000120374656e",
                                      "4a0e0d0f00000012076669667465656e"),
              result.toHex());
@@ -42,7 +61,7 @@ void QtProtobufMapTypesSerializationTest::SimpleSFixed32StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleInt32StringMapSerializeTest()
 {
     SimpleInt32StringMapMessage test;
-    test.setMapField({{-10, {"minus ten"}}, {42, {"fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { -10, { "minus ten" } }, { 42, { "fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
     QVERIFY2(compareSerializedChunks(result.toHex(),
@@ -55,24 +74,21 @@ void QtProtobufMapTypesSerializationTest::SimpleInt32StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleSInt32StringMapSerializeTest()
 {
     SimpleSInt32StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {-42, {"minus fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { -42, { "minus fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "0a14085312106d696e757320666f757274792074776f",
-                                     "0a0b081e12076669667465656e",
-                                     "0a070814120374656e"),
+    QVERIFY2(compareSerializedChunks(result.toHex(), "0a14085312106d696e757320666f757274792074776f",
+                                     "0a0b081e12076669667465656e", "0a070814120374656e"),
              result.toHex());
 }
 
 void QtProtobufMapTypesSerializationTest::SimpleUInt32StringMapSerializeTest()
 {
     SimpleUInt32StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {42, {"fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { 42, { "fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "2a07080a120374656e",
+    QVERIFY2(compareSerializedChunks(result.toHex(), "2a07080a120374656e",
                                      "2a0b080f12076669667465656e",
                                      "2a0e082a120a666f757274792074776f"),
              result.toHex());
@@ -81,11 +97,10 @@ void QtProtobufMapTypesSerializationTest::SimpleUInt32StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleFixed64StringMapSerializeTest()
 {
     SimpleFixed64StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {42, {"fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { 42, { "fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "420e090a00000000000000120374656e",
+    QVERIFY2(compareSerializedChunks(result.toHex(), "420e090a00000000000000120374656e",
                                      "4212090f0000000000000012076669667465656e",
                                      "4215092a00000000000000120a666f757274792074776f"),
              result.toHex());
@@ -94,7 +109,7 @@ void QtProtobufMapTypesSerializationTest::SimpleFixed64StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleSFixed64StringMapSerializeTest()
 {
     SimpleSFixed64StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {-42, {"minus fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { -42, { "minus fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
     QVERIFY2(compareSerializedChunks(result.toHex(),
@@ -107,7 +122,7 @@ void QtProtobufMapTypesSerializationTest::SimpleSFixed64StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleInt64StringMapSerializeTest()
 {
     SimpleInt64StringMapMessage test;
-    test.setMapField({{-10, {"minus ten"}}, {42, {"fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { -10, { "minus ten" } }, { 42, { "fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
     QVERIFY2(compareSerializedChunks(result.toHex(),
@@ -120,23 +135,20 @@ void QtProtobufMapTypesSerializationTest::SimpleInt64StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleSInt64StringMapSerializeTest()
 {
     SimpleSInt64StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {-42, {"minus fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { -42, { "minus fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "1214085312106d696e757320666f757274792074776f",
-                                     "12070814120374656e",
-                                     "120b081e12076669667465656e"),
+    QVERIFY2(compareSerializedChunks(result.toHex(), "1214085312106d696e757320666f757274792074776f",
+                                     "12070814120374656e", "120b081e12076669667465656e"),
              result.toHex());
 }
 
 void QtProtobufMapTypesSerializationTest::SimpleUInt64StringMapSerializeTest()
 {
     SimpleUInt64StringMapMessage test;
-    test.setMapField({{10, {"ten"}}, {42, {"fourty two"}}, {15, {"fifteen"}}});
+    test.setMapField({ { 10, { "ten" } }, { 42, { "fourty two" } }, { 15, { "fifteen" } } });
     QByteArray result = test.serialize(m_serializer.get());
 
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "3207080a120374656e",
+    QVERIFY2(compareSerializedChunks(result.toHex(), "3207080a120374656e",
                                      "320b080f12076669667465656e",
                                      "320e082a120a666f757274792074776f"),
              result.toHex());
@@ -145,16 +157,17 @@ void QtProtobufMapTypesSerializationTest::SimpleUInt64StringMapSerializeTest()
 void QtProtobufMapTypesSerializationTest::SimpleStringStringMapSerializeTest()
 {
     SimpleStringStringMapMessage test;
-    test.setMapField({{"ben", "ten"}, {"what is the answer?", "fourty two"}, {"sweet", "fifteen"}});
+    test.setMapField({ { "ben", "ten" },
+                       { "what is the answer?", "fourty two" },
+                       { "sweet", "fifteen" } });
     QByteArray result = test.serialize(m_serializer.get());
 
-    QVERIFY2(compareSerializedChunks(result.toHex(),
-                                     "6a0a0a0362656e120374656e",
-                                     "6a100a05737765657412076669667465656e",
-                                     "6a210a13776861742069732074686520616e737765723f120a666f757274792074776f"),
+    QVERIFY2(compareSerializedChunks(
+                     result.toHex(), "6a0a0a0362656e120374656e",
+                     "6a100a05737765657412076669667465656e",
+                     "6a210a13776861742069732074686520616e737765723f120a666f757274792074776f"),
              result.toHex());
 }
 
 QTEST_MAIN(QtProtobufMapTypesSerializationTest)
-
-#include "moc_tst_protobuf_serialization_maptypes.cpp"
+#include "tst_protobuf_serialization_maptypes.moc"
