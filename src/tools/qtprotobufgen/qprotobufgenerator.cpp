286258f8ae51c3959a85cadcf370ae9ce04ea22d
Konrad Kujawa <konrad.kujawa@qt.io>
2023-06-22 15:33:11 +0200

    Remove protobuf logging.h include
    This include was removed in the new version of the protobuf,
    and it was unused anyway.
    
    Change-Id: I5c68f960c031d7f366ab3dc1b690dc9050369975
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 1a610ab9c7edde2e27ff707c9e534172e288e8ed)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/tools/qtprotobufgen/qprotobufgenerator.cpp
+++ src/tools/qtprotobufgen/qprotobufgenerator.cpp
@@ -17,7 +17,6 @@
 #include <numeric>
 #include <unordered_set>
 
-#include <google/protobuf/stubs/logging.h>
 #include <google/protobuf/stubs/common.h>
 #include <google/protobuf/io/printer.h>
 #include <google/protobuf/io/zero_copy_stream.h>
