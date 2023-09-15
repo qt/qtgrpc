202f4b4ebdd11118c2f0afad0516c2baea9ab07a
Alexey Edelev <alexey.edelev@qt.io>
2023-09-15 13:18:38 +0200

    Use the 'stubs/common.h' header as the source of protobuf version
    This header exists in both new and very old libprotoc versions.
    
    Pick-to: 6.5 6.6 6.6.0
    Change-Id: I43b371576bbbf4db440db33cd83b29e3f305149c
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Konrad Kujawa <konrad.kujawa@qt.io>

--- src/tools/qtprotoccommon/qtprotocdefs.h
+++ src/tools/qtprotoccommon/qtprotocdefs.h
@@ -4,11 +4,10 @@
 #ifndef QTPROTOCDEFS_H
 #define QTPROTOCDEFS_H
 
-#include <google/protobuf/port_def.inc>
-#if PROTOBUF_VERSION >= 3012000
+#include <google/protobuf/stubs/common.h>
+#if GOOGLE_PROTOBUF_VERSION >= 3012000
 // Idicates the major libprotoc API update that happened in version 3.12
 #  define HAVE_PROTOBUF_SYNC_PIPER
 #endif
-#include <google/protobuf/port_undef.inc>
 
 #endif // QTPROTOCDEFS_H
