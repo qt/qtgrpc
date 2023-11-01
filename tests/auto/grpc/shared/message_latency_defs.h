0f13dd0d5b9a4ee46e563b0ff9500e6067e69f5e
Tatiana Borisova <tatiana.borisova@qt.io>
2023-11-01 14:34:25 +0100

    Fix QtGrpcClientServerStreamTest test
    - revert 51873eb639fe54c026dd49cf13feb01406ff1555 change,
    but increased LATENCY for WINDOWS;
    
    Change-Id: I182ae80bd528afc12b79a2dc9785aebf1a824cfa
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 5ec9870903dae92d3c4be5253f4c75f9091508ec)
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>

--- tests/auto/grpc/shared/message_latency_defs.h
+++ tests/auto/grpc/shared/message_latency_defs.h
@@ -4,11 +4,19 @@
 #define MESSAGE_LATENCY_DEFS_H
 
 #ifndef QT_GRPC_TEST_MESSAGE_LATENCY
+#ifdef Q_OS_WINDOWS
+#  define QT_GRPC_TEST_MESSAGE_LATENCY 1000
+#else
 #  define QT_GRPC_TEST_MESSAGE_LATENCY 300
+#endif
 #endif
 
 constexpr int MessageLatency = QT_GRPC_TEST_MESSAGE_LATENCY;
+#ifdef Q_OS_WINDOWS
+constexpr int MessageLatencyThreshold = MessageLatency / 3; // we also need ~300 for WIN
+#else
 constexpr int MessageLatencyThreshold = MessageLatency / 5;
+#endif
 constexpr int MessageLatencyWithThreshold = MessageLatency + MessageLatencyThreshold;
 constexpr int FailTimeout = QT_GRPC_TEST_MESSAGE_LATENCY * 5;
 
