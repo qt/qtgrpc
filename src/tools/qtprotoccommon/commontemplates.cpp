242071cd695c04835b52c1d5fdedb4e7272308e2
Tatiana Borisova <tatiana.borisova@qt.io>
2023-11-09 17:37:11 +0100

    Fix Memory leak in in qprotobufserializer.h
    - Add deleting of pointer in the setter function
    
    Fixes: QTBUG-118996
    Change-Id: I7322fb049f9dec493c03ddb9180eb0d4ec57c06e
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit e9c80d43f72a5e92e19536e574f0cef794ef0cb4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/tools/qtprotoccommon/commontemplates.cpp
+++ src/tools/qtprotoccommon/commontemplates.cpp
@@ -689,6 +689,7 @@ const char *CommonTemplates::PrivateSetterOneofMessageDefinitionTemplate()
            "        dptr.detach();\n"
            "        dptr->m_$optional_property_name$.setValue(value, $number$);\n"
            "    }\n"
+           "    delete $property_name$;\n"
            "}\n\n";
 }
 
