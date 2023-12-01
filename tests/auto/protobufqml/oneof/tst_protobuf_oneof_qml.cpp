2519d8f6e44ecd91f0b0b7306e633b93540ecebc
Alexey Edelev <alexey.edelev@qt.io>
2023-12-01 15:05:45 +0100

    Add QML tests for oneof fields
    Change-Id: If4254660471240c5be75e9b4f4a3fcc283bdb348
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>

---
+++ tests/auto/protobufqml/oneof/tst_protobuf_oneof_qml.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include <QtQuickTest>
+#include <QtProtobuf/qtprotobuftypes.h>
+
+QT_BEGIN_NAMESPACE
+class QQmlEngine;
+QT_END_NAMESPACE
+
+class TestSetup : public QObject {
+    Q_OBJECT
+
+public slots:
+    void qmlEngineAvailable(QQmlEngine *)
+    {
+        qRegisterProtobufTypes();
+    }
+};
+
+QUICK_TEST_MAIN_WITH_SETUP(tst_protobuf_oneof_qml, TestSetup)
+#include "tst_protobuf_oneof_qml.moc"
