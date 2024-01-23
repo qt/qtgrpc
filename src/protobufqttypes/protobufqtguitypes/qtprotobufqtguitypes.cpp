89da5152131c3e6f12b40afa703b76119c874214
Alexey Edelev <alexey.edelev@qt.io>
2024-01-23 08:07:32 +0100

    Fix the license header in the qtprotobuf<core|gui>types modules
    Pick-to: 6.6
    Change-Id: I53b3642fdb32ccd8d857e205ff3b1db31464c6df
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    (cherry picked from commit 2e7a71983c9186bb2cf30fc62eebc05d7c617556)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/protobufqttypes/protobufqtguitypes/qtprotobufqtguitypes.cpp
+++ src/protobufqttypes/protobufqtguitypes/qtprotobufqtguitypes.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // Copyright (C) 2019 Alexey Edelev <semlanik@gmail.com>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qtprotobufqtguitypes.h"
 #include "private/QtGui.qpb.h"
