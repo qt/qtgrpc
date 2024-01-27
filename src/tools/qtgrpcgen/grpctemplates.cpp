3e541cd7793f4e2da7aa97f0026f4f28b8964258
Alexey Edelev <alexey.edelev@qt.io>
2024-01-27 08:22:32 +0100

    Avoid constructing redundant copies in gRPC calls
    Constructing a copy of QJSValue for gRPC callbacks look redundant.
    Remove it. Same applicable for the result.
    
    Pick-to: 6.6 6.7
    Change-Id: I3b5c81e8373ca85ccd66e65555305c9dba020a6c
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/tools/qtgrpcgen/grpctemplates.cpp
+++ src/tools/qtgrpcgen/grpctemplates.cpp
@@ -107,10 +107,10 @@ const char *GrpcTemplates::ClientMethodDefinitionQmlTemplate()
            "    std::shared_ptr<QGrpcCallReply> reply = call(\"$method_name$\"_L1, "
            "$param_name$, options);\n"
            "    reply->subscribe(jsEngine, [reply, callback, jsEngine]() {\n"
-           "        auto result = $return_type$(reply->read<$return_type$>());\n"
-           "        QJSValue(callback).call(QJSValueList{jsEngine->toScriptValue(result)});\n"
+           "        auto result = reply->read<$return_type$>();\n"
+           "        callback.call(QJSValueList{jsEngine->toScriptValue(result)});\n"
            "    }, [errorCallback, jsEngine](const QGrpcStatus &status) {\n"
-           "        QJSValue(errorCallback).call(QJSValueList{jsEngine->toScriptValue(status)});\n"
+           "        errorCallback.call(QJSValueList{jsEngine->toScriptValue(status)});\n"
            "    });\n"
            "}\n";
 }
