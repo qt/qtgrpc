ab86f0f755377edac1224d293094592905b3ad9e
Alexey Edelev <alexey.edelev@qt.io>
2024-04-05 16:17:08 +0200

    Remove the unused testservice_protobuftyperegistrations.cpp
    Remove the unused testservice_protobuftyperegistrations.cpp from
    the grpcgen test data. The file is not used in testing.
    
    Pick-to: 6.7
    Change-Id: I002f1b5bcdd454550d3ce4ff48f52153d68eb537
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- tests/auto/grpcgen/data/expected_result/folder/qtgrpc/tests/testservice_protobuftyperegistrations.cpp
+++
@@ -1,9 +0,0 @@
-#include <QtProtobuf/qprotobufregistration.h>
-#include "qtgrpc/tests/testservice.qpb.h"
-
-namespace qtgrpc::tests {
-static QtProtobuf::ProtoTypeRegistrar ProtoTypeRegistrarSimpleStringMessage(qRegisterProtobufType<SimpleStringMessage>);
-static QtProtobuf::ProtoTypeRegistrar ProtoTypeRegistrarSimpleIntMessage(qRegisterProtobufType<SimpleIntMessage>);
-static QtProtobuf::ProtoTypeRegistrar ProtoTypeRegistrarBlobMessage(qRegisterProtobufType<BlobMessage>);
-} // namespace qtgrpc::tests
-
