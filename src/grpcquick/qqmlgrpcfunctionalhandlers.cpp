f0d9c251e5afb8c8e9cea2406147f60b7c22c1fd
Alexey Edelev <alexey.edelev@qt.io>
2024-07-14 22:39:33 +0200

    Move the QGrpcOperation::finished signal connection to the non-inline function
    The functionality doesn't depend on the template arguments so we may
    move it out of template function body.
    
    Pick-to: 6.8
    Task-number: QTBUG-120943
    Change-Id: I80f402d43bfc88d109d3c9170ed01eebb4e207d8
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>

---
+++ src/grpcquick/qqmlgrpcfunctionalhandlers.cpp
@@ -0,0 +1,38 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QtGrpcQuick/qqmlgrpcfunctionalhandlers.h>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtGrpcQuickFunctional {
+
+void connectMultipleReceiveOperationFinished(QJSEngine *jsEngine,
+                                             const std::shared_ptr<QGrpcOperation> &operation,
+                                             const QJSValue &successCallback,
+                                             const QJSValue &errorCallback)
+{
+    Q_ASSERT(jsEngine != nullptr);
+    Q_ASSERT(operation);
+
+    auto finishConnection = std::make_shared<QMetaObject::Connection>();
+    *finishConnection = QObject::connect(operation.get(), &QGrpcOperation::finished, jsEngine,
+                                         [successCallback, errorCallback, jsEngine,
+                                          finishConnection,
+                                          operation](const QGrpcStatus &status) {
+                                             // We take 'operation' by copy so that its lifetime
+                                             // is extended until this lambda is destroyed.
+                                             if (status.code() == QtGrpc::StatusCode::Ok) {
+                                                 if (successCallback.isCallable())
+                                                     successCallback.call();
+                                             } else if (errorCallback.isCallable()) {
+                                                 errorCallback.call(QJSValueList{
+                                                     jsEngine->toScriptValue(status) });
+                                             }
+                                             QObject::disconnect(*finishConnection);
+                                         });
+}
+
+} // namespace QtGrpcQuickFunctional
+
+QT_END_NAMESPACE
