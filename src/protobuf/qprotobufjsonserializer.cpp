dbfaf7429dbde5964577149bb95966aa166e56b2
Alexey Edelev <alexey.edelev@qt.io>
2024-08-21 16:58:32 +0200

    Make QtProtobufPrivate::findHandler private API
    The function is used by serializer implementations to find a custom
    serializer for the specific field. We shouldn't expose it as public
    API with no purpose. Move to the private header and export for
    further use without binary compatibility promises.
    
    Task-number: QTBUG-123626
    Change-Id: I755c0a9709519707051d4b070395b31b6284767b
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>
    (cherry picked from commit 56778a55e84c663672df68df3801137a6458f1c3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/protobuf/qprotobufjsonserializer.cpp
+++ src/protobuf/qprotobufjsonserializer.cpp
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qprotobufjsonserializer.h"
+#include "qprotobufregistration_p.h"
 #include "qprotobufserializer_p.h"
-#include "qprotobufregistration.h"
 #include "qtprotobufdefs_p.h"
 
 #include <QtCore/qcoreapplication.h>
