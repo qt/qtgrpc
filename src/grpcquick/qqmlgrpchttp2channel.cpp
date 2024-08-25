c1850ae890b664772ad0b88be7cc91c7c90d9372
Alexey Edelev <alexey.edelev@qt.io>
2024-08-25 14:27:29 +0200

    Make the QQmlGrpcHttp2Channel options property non-required
    Allow initializing the QQmlGrpcHttp2Channel instances with no
    QQmlGrpcChannelOptions specified.
    
    Fix the options assignment and update mechanism as drive-by
    change.
    
    Task-number: QTBUG-123625
    Task-number: QTBUG-126286
    Change-Id: I771073674ec4d39876bc3d87fa4a0d3a06d98be3
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>
    (cherry picked from commit 2e5850fecefd2dc9628a674bf7f15de8f9315462)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/grpcquick/qqmlgrpchttp2channel.cpp
+++ src/grpcquick/qqmlgrpchttp2channel.cpp
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #include <QtGrpcQuick/qqmlabstractgrpcchannel.h>
+
 #include <QtGrpcQuick/private/qqmlgrpchttp2channel_p.h>
+#include <QtGrpcQuick/private/qqmlabstractgrpcchannel_p.h>
 
 #include <QtGrpc/qgrpchttp2channel.h>
 
@@ -10,63 +12,126 @@
 
 QT_BEGIN_NAMESPACE
 
-QQmlGrpcHttp2Channel::QQmlGrpcHttp2Channel(QObject *parent)
-    : QQmlAbstractGrpcChannel(parent)
+namespace {
+#if QT_CONFIG(ssl)
+constexpr size_t QQmlGrpcChannelOptionsPropertiesCount = 4;
+#else
+constexpr size_t QQmlGrpcChannelOptionsPropertiesCount = 3;
+#endif
+}
+
+class QQmlGrpcHttp2ChannelPrivate : public QQmlAbstractGrpcChannelPrivate
 {
-    connect(this, &QQmlGrpcHttp2Channel::optionsChanged,
-            this, &QQmlGrpcHttp2Channel::updateChannel);
+public:
+    QQmlGrpcChannelOptions *options = nullptr;
+    QUrl hostUri;
+    std::shared_ptr<QAbstractGrpcChannel> channel;
+    std::array<QMetaObject::Connection, QQmlGrpcChannelOptionsPropertiesCount> optionsConnections;
+};
+
+QQmlGrpcHttp2Channel::QQmlGrpcHttp2Channel(QObject *parent)
+    : QQmlAbstractGrpcChannel(*(new QQmlGrpcHttp2ChannelPrivate), parent)
+{
 }
 
 QQmlGrpcHttp2Channel::~QQmlGrpcHttp2Channel() = default;
 
 std::shared_ptr<QAbstractGrpcChannel> QQmlGrpcHttp2Channel::channel()
 {
-    return m_channel;
+    Q_D(const QQmlGrpcHttp2Channel);
+    return d->channel;
 }
 
 void QQmlGrpcHttp2Channel::setOptions(QQmlGrpcChannelOptions *options)
 {
-    if (options == nullptr || m_options == options)
+    Q_D(QQmlGrpcHttp2Channel);
+    if (d->options == options)
         return;
 
-    m_options = options;
-    emit optionsChanged();
+    for (auto &connection : d->optionsConnections) {
+        if (connection) {
+            disconnect(connection);
+            connection = {};
+        }
+    }
+
+    d->options = options;
+    if (d->options) {
+        auto updateChannelOptions = [this]() {
+            Q_D(QQmlGrpcHttp2Channel);
+            if (d->channel) {
+                d->channel->setChannelOptions(d->options != nullptr ? d->options->options()
+                                                                    : QGrpcChannelOptions{});
+            }
+            emit optionsChanged();
+        };
+
+        d->optionsConnections[0] = connect(d->options,
+                                           &QQmlGrpcChannelOptions::deadlineTimeoutChanged, this,
+                                           updateChannelOptions);
+        d->optionsConnections[1] = connect(d->options, &QQmlGrpcChannelOptions::metadataChanged,
+                                           this, updateChannelOptions);
+        d->optionsConnections[2] = connect(d->options,
+                                           &QQmlGrpcChannelOptions::serializationFormatChanged,
+                                           this, updateChannelOptions);
+#if QT_CONFIG(ssl)
+        d->optionsConnections[3] = connect(d->options,
+                                           &QQmlGrpcChannelOptions::sslConfigurationChanged, this,
+                                           updateChannelOptions);
+#endif
+        updateChannelOptions();
+    }
 }
 
 void QQmlGrpcHttp2Channel::updateChannel()
 {
-    if (m_hostUri.isEmpty())
+    Q_D(QQmlGrpcHttp2Channel);
+    if (d->hostUri.isEmpty())
         return;
 
-    if (!m_hostUri.isValid()) {
+    if (!d->hostUri.isValid()) {
         qWarning() << "Unable to initialize the channel. The host URI is not valid.";
         return;
     }
 
-    if (m_channel)
-        m_channel.reset();
+    if (d->channel)
+        d->channel.reset();
 
-    if (m_hostUri.isValid()) {
-        m_channel = m_options ? std::make_shared<QGrpcHttp2Channel>(m_hostUri, m_options->options())
-                              : std::make_shared<QGrpcHttp2Channel>(m_hostUri);
+    if (d->hostUri.isValid()) {
+        d->channel = d->options
+            ? std::make_shared<QGrpcHttp2Channel>(d->hostUri, d->options->options())
+            : std::make_shared<QGrpcHttp2Channel>(d->hostUri);
     }
     emit channelUpdated();
 }
 
 void QQmlGrpcHttp2Channel::setHostUri(const QUrl &hostUri)
 {
-    if (hostUri == m_hostUri)
+    Q_D(QQmlGrpcHttp2Channel);
+    if (hostUri == d->hostUri)
         return;
 
-    if (m_channel) {
+    if (d->channel) {
         qWarning() << "Changing the host URI is not supported.";
         return;
     }
 
-    m_hostUri = hostUri;
+    d->hostUri = hostUri;
 
     emit hostUriChanged();
     updateChannel();
+}
+
+QQmlGrpcChannelOptions *QQmlGrpcHttp2Channel::options() const noexcept
+{
+    Q_D(const QQmlGrpcHttp2Channel);
+    return d->options;
+}
+
+QUrl QQmlGrpcHttp2Channel::hostUri() const noexcept
+{
+    Q_D(const QQmlGrpcHttp2Channel);
+    return d->hostUri;
 }
 
 QT_END_NAMESPACE
