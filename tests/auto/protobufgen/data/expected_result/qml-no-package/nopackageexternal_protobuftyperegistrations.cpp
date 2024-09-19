a07ab5b389a8067e44660e275dd761c3a96482f4
Alexey Edelev <alexey.edelev@qt.io>
2024-09-19 23:52:52 +0200

    Refactor the qtprotobufgen test
    Use the common .proto scheme for all testcases, that defined lot of
    message variants, so we can see how changes in generator affect
    various types but not only restricted subset.
    
    Rearrange the test cases according to the generator options.
    
    Change the verification process for command line tests. Consider not
    only some files but all generated files, so we can see if some new
    file is added or some files are remove/renamed.
    
    Simplify tests.
    
    Add the COPY_TEST_RESULTS environment variable support. This variable
    now allows copying test results to the source directory. The introduced
    functionality is very handy when need update expected results after the
    major changes in generator.
    
    Add the respective feature guards for the QProcess usage. In case if
    the feature is disabled in QtCore.
    
    Task-number: QTBUG-128753
    Change-Id: I5ac33684c8cf4fd5dd133b404061db4435a87344
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>
    (cherry picked from commit ce98c06105755c0d2e78b9056541d79b239580aa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/protobufgen/data/expected_result/qml-no-package/nopackageexternal_protobuftyperegistrations.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "nopackageexternal.qpb.h"
-
-#include <QtProtobuf/qprotobufregistration.h>
-
-static QtProtobuf::ProtoTypeRegistrar ProtoTypeRegistrarSimpleIntMessageExt(qRegisterProtobufType<SimpleIntMessageExt>);
-static bool RegisterNopackageexternalProtobufTypes = [](){ qRegisterProtobufTypes(); return true; }();
-
