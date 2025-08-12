9370589e094318a24e281eae656fec45c65bd121
Alexey Edelev <alexey.edelev@qt.io>
2025-08-12 14:57:36 +0200

    Allow enums be non-packed
    Repeated enum fields should not ignore the 'packed' protobuf attribute,
    so if 'packed' is set to 'false', FieldFlag::NonPacked should land to
    the message field attributes, indicating to serializer the expected
    output format.
    
    Fixes: QTBUG-138683
    Pick-to: 6.9 6.8
    Change-Id: I1c2d2bee32b42f60310307e1f3638ba2b3c236a3
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>
    (cherry picked from commit c764a9b223329cba6b10f60f3fd9ccd7b4446f46)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/protobufgen/data/cmake_generated/export_macro/qtprotobufgen.qpb.h
+++ tests/auto/protobufgen/data/cmake_generated/export_macro/qtprotobufgen.qpb.h
@@ -457,6 +457,7 @@ class ScalarRepeatedNoPackedTypes : public QProtobufMessage
     Q_PROPERTY(QtProtobuf::doubleList field12 READ field12 WRITE setField12 SCRIPTABLE true)
     Q_PROPERTY(QStringList field13 READ field13 WRITE setField13 SCRIPTABLE true)
     Q_PROPERTY(QByteArrayList field14 READ field14 WRITE setField14 SCRIPTABLE true)
+    Q_PROPERTY(QList<qt::protobuf::GlobalEnumGadget::GlobalEnum> field15 READ field15 WRITE setField15 SCRIPTABLE true)
 
 public:
     using QtProtobufFieldEnum = ScalarRepeatedNoPackedTypes_QtProtobufNested::QtProtobufFieldEnum;
@@ -504,6 +505,8 @@ public:
     QPB_CUSTOM_EXPORT_NAME_EXPORT const QStringList &field13() const &;
 
     QPB_CUSTOM_EXPORT_NAME_EXPORT const QByteArrayList &field14() const &;
+
+    QPB_CUSTOM_EXPORT_NAME_EXPORT const QList<GlobalEnumGadget::GlobalEnum> &field15() const &;
     QPB_CUSTOM_EXPORT_NAME_EXPORT void setField1(const QtProtobuf::int32List &field1);
     QPB_CUSTOM_EXPORT_NAME_EXPORT void setField1(QtProtobuf::int32List &&field1);
     QPB_CUSTOM_EXPORT_NAME_EXPORT void setField2(const QtProtobuf::uint32List &field2);
@@ -532,6 +535,8 @@ public:
     QPB_CUSTOM_EXPORT_NAME_EXPORT void setField13(QStringList &&field13);
     QPB_CUSTOM_EXPORT_NAME_EXPORT void setField14(const QByteArrayList &field14);
     QPB_CUSTOM_EXPORT_NAME_EXPORT void setField14(QByteArrayList &&field14);
+    QPB_CUSTOM_EXPORT_NAME_EXPORT void setField15(const QList<GlobalEnumGadget::GlobalEnum> &field15);
+    QPB_CUSTOM_EXPORT_NAME_EXPORT void setField15(QList<GlobalEnumGadget::GlobalEnum> &&field15);
     QPB_CUSTOM_EXPORT_NAME_EXPORT static void registerTypes();
 
 private:
@@ -564,6 +569,7 @@ enum class QtProtobufFieldEnum {
     Field12ProtoFieldNumber = 12,
     Field13ProtoFieldNumber = 13,
     Field14ProtoFieldNumber = 14,
+    Field15ProtoFieldNumber = 15,
 };
 Q_ENUM_NS(QtProtobufFieldEnum)
 
