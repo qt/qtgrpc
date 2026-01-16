b7c384f853a7acf87ca6031d9b341832b9d08284
Dennis Oberst <dennis.oberst@qt.io>
2026-01-16 13:28:39 +0100

    QGrpc{Call,Channel}Options: remove qHash and QDataStream overloads
    ... as they are currently not qualified for integration with these
    classes. Future Qt integrations for these types will be provided and
    controlled by the authors as needed.
    
    Found during the Qt 6.11 API review.
    
    Pick-to: 6.11
    Change-Id: If5860dbefc4289b7bc9419dcbcc27e2954586c96
    Reviewed-by: Alexey Edelev <semlanik@gmail.com>

--- src/grpc/qgrpcchanneloptions.h
+++ src/grpc/qgrpcchanneloptions.h
@@ -23,6 +23,7 @@
 
 QT_BEGIN_NAMESPACE
 
+class QDataStream;
 class QDebug;
 class QVariant;
 class QGrpcSerializationFormat;
@@ -91,8 +92,15 @@ private:
                                             const QGrpcChannelOptions &rhs);
     Q_DECLARE_EQUALITY_COMPARABLE_NON_NOEXCEPT(QGrpcChannelOptions)
 
+    friend size_t qHash(const QGrpcChannelOptions &key) = delete;
+    friend size_t qHash(const QGrpcChannelOptions &key, size_t seed) = delete;
+
 #ifndef QT_NO_DEBUG_STREAM
     friend Q_GRPC_EXPORT QDebug operator<<(QDebug debug, const QGrpcChannelOptions &chOpts);
+#endif
+#ifndef QT_NO_DATASTREAM
+    friend QDataStream &operator<<(QDataStream &out, const QGrpcChannelOptions &chOpts) = delete;
+    friend QDataStream &operator>>(QDataStream &in, QGrpcChannelOptions &chOpts) = delete;
 #endif
 
     Q_DECLARE_PRIVATE(QGrpcChannelOptions)
